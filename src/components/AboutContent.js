3ecde361dd5dfc7544063694258d609aa558c554
PRTAEEK THAPLIYAL <99065076+PRATEEKcode101@users.noreply.github.com>
2023-05-30 10:49:50 +0530

    Add files via upload

---
+++ src/components/AboutContent.js
@@ -0,0 +1,41 @@
+import "./AboutContentStyles.css"
+
+import React from 'react'
+import { Link } from "react-router-dom"
+import React1 from "../assets/React.jpg"
+import React2 from "../assets/Prateek.webp"
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+      <div className="left">
+        <h1>Who Am I?</h1>
+        <p>
+            I'm a Full Stack Web-developer.
+            I've just completed my development course.
+            I'm working on so many new projects right now
+            have a look on them in the project section !! 
+        </p>
+        <Link to="/contact">
+            <button className="btn">Contact</button>
+        </Link>
+      </div>
+      <div className="right">
+        <div className="img-container">
+            <div className="img-stack top">
+                <img src={React1} className="img" alt="true" />
+            </div>
+            <div className="img-stack bottom">
+                <img src={React2} className="img" alt="true" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export const Skills = () =>{
+
+}
+
+
+export default AboutContent
---
+++ src/components/HeroImage.js
@@ -0,0 +1,26 @@
+import "./HeroImageStyles.css";
+
+import React from 'react'
+
+import  IntroImg from "../assets/pic.jpg"
+import { Link } from "react-router-dom";
+
+const HeroImage = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="into-img" src={IntroImg} alt="IntroImg"/>
+        </div>
+        <div className="content">
+            <p>HI, I'M A FULL STACK WEB DEVELOPER.</p>
+            <h1>React Developer.</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn-light">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImage
